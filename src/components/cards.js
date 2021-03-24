6cddd2e6be27006e423886675e7c22b94124ef3f
Lucas Rocha <rocha.lucas28@gmail.com>
2021-03-23 21:56:58 -0300

    ScroolView on Main page

---
+++ src/components/cards.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {TitleFrom} from '../styles/globalStyles';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const MCards = () => {
+  const renderItem = ({item}) => <Item title={item.title} />;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TitleFrom>Categoria</TitleFrom>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        horizontal
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    height: 180,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default MCards;
--- src/pages/blank.js
+++ src/pages/blank.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import BoxParty from '../components/boxParty';
-import styled from 'styled-components/native';
+import MCards from '../components/cards';
+import {ScrollView} from 'react-native';
+// import styled from 'styled-components/native';
 
 import {Container, Footer, TitleFooter} from '../styles/globalStyles';
 
 export default function Blank({navigation}) {
   return (
     <Container>
-      <Cards>
-        <BoxParty />
-        <BoxParty />
-        <BoxParty />
-      </Cards>
-
-      <Footer
-        onPress={() => navigation.navigate('Register')}
-        activeOpacity={0.7}>
+      <ScrollView>
+        <MCards />
+        <MCards />
+        <MCards />
+        <MCards />
+      </ScrollView>
+      <Footer onPress={() => navigation.navigate('Login')} activeOpacity={0.7}>
         {/* <IconLogin name="login" color="#404040" size={scaleFontSize(14)} /> */}
-        <TitleFooter>Voltar</TitleFooter>
+        <TitleFooter>Sair</TitleFooter>
       </Footer>
     </Container>
   );
 }
-
-export const Cards = styled.View`
-  flex: 1;
-  width: 100%;
-`;
